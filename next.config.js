16eff049b14ebf53a7228d548a3b0d9d5ff5d265
peeweeid <65776660+peeweeid@users.noreply.github.com>
2023-05-10 14:53:05 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['raw.githubusercontent.com'],
+  }
 }
 const withImages = require("next-images");
 const withTM = require("next-transpile-modules")(["@madzadev/image-slider"]);
 module.exports = nextConfig
-module.exports = withImages(withTM());
+
